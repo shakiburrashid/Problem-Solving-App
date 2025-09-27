20b35e92815750a63b71fe874490055d0e1e6154
shakiburrashid <shakiburrashid7@gmail.com>
2025-09-27 21:17:22 +0600

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
 
 
-        <Navbar />
+        {/* <Navbar /> */}
 
         {/* <Routes>
           <Route path="/" element={<Home />} />
@@ -23,7 +23,47 @@ function App() {
         </Routes>
          */}
         <section className="px-10 py-10 font-roboto">
-          <Banner/>
+          {/* <Banner/> */}
+
+          <div>
+            <div className="border px-10 py-5 w-150 h-70 rounded-md">
+              <div className="flex justify-between items-center">
+                <h1 className="text-2xl">Lorem ipsum dolor sit amet.</h1>
+                <div className="flex gap-3 items-center bg-green-200 h-8 w-25 rounded-2xl p-2">
+                  <i className="fa-solid fa-circle text-green-500"></i>
+                  <h1 className="text-green-600">Open</h1>
+                </div>
+              </div>
+              <div className="mt-5">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus enim sunt hic numquam veniam ullam accusantium inventore sint, repellat sed.</p>
+                <div className="flex mt-5 justify-between items-center">
+                  <div className="flex gap-5">
+                    <p>1051</p>
+                    <h3><span>HIGH</span> PRIORITY</h3>
+                  </div>
+                  <div className="flex gap-5">
+                    <h2>Name</h2>
+                    <i className="fa-solid fa-calendar"></i>
+                    <h1>10/5/2025</h1>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         </section>
       </BrowserRouter>
---
+++ src/Components/Card/CardGet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardGet() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CardGet
