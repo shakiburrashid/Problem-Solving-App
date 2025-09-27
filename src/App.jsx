57fd6f3d31104d80165fbe4cbf252f6963c940ae
shakiburrashid <shakiburrashid7@gmail.com>
2025-09-27 14:06:24 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+   <>
+    <section className="px-15 pt-10">
+      <div>
+        <div>
+          <h1 className="font-roboto text-3xl font-bold"><span> Problem</span><span> Soliving</span><span> App</span></h1>
+        </div>
+      </div>
+    </section>
+   </>
+  )
+}
+
+export default App
