4a9172481dd2ad913dbcb60ce948803cdfa947a8
shakiburrashid <shakiburrashid7@gmail.com>
2025-09-27 22:06:29 +0600

    card created with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -26,45 +26,32 @@ function App() {
           {/* <Banner/> */}
 
           <div>
-            <div className="border px-10 py-5 w-150 h-70 rounded-md">
+            <div className=" px-7 py-5 w-150 h-50 rounded-xl shadow-sm max-2xl:w-120 max-2xl:h-43 max-2xl:p-3 max-md:w-80 max-md:h-65 max-md:place-content-center">
               <div className="flex justify-between items-center">
-                <h1 className="text-2xl">Lorem ipsum dolor sit amet.</h1>
+                <h1 className="text-2xl max-2xl:text-xl">Lorem ipsum dolor sit amet.</h1>
                 <div className="flex gap-3 items-center bg-green-200 h-8 w-25 rounded-2xl p-2">
                   <i className="fa-solid fa-circle text-green-500"></i>
                   <h1 className="text-green-600">Open</h1>
                 </div>
               </div>
-              <div className="mt-5">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus enim sunt hic numquam veniam ullam accusantium inventore sint, repellat sed.</p>
-                <div className="flex mt-5 justify-between items-center">
-                  <div className="flex gap-5">
-                    <p>1051</p>
-                    <h3><span>HIGH</span> PRIORITY</h3>
+              <div className="mt-3">
+                <p className="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus enim sunt hic numquam veniam ullam accusantium inventore sint, repellat sed.</p>
+                <div className="flex mt-5 justify-between items-center max-md:flex-col max-md:gap-3">
+                  <div className="flex gap-5 items-center  max-md:gap-35">
+                    <p className="text-xl text-gray-500  max-2xl:text-sm"># <span>1051</span></p>
+                    <h3 className="text-xl text-red-500 font-semibold max-2xl:text-sm"><span>HIGH</span> PRIORITY</h3>
                   </div>
-                  <div className="flex gap-5">
-                    <h2>Name</h2>
-                    <i className="fa-solid fa-calendar"></i>
-                    <h1>10/5/2025</h1>
+                  <div className="flex gap-3 items-center text-xl text-gray-500 max-2xl:text-sm max-md:gap-30">
+                    <h2>Lorem, ipsum </h2>
+                    <div className="flex gap-1 items-center">
+                      <i className="fa-solid fa-calendar"></i>
+                      <h1>10/5/2025</h1>
+                    </div>
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </section>
       </BrowserRouter>
 
